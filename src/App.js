d024edcbbf464d2c75123f62bcd8f64a00d11c2f
Sergey Ganin <mcheguevara2@gmail.com>
2019-07-18 20:11:35 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <a target="_blank" href="https://material-ui.com/api/icon/">
         https://material-ui.com/api/icon/
       </a>
+      <a target="_blank" href="https://material-ui.com/components/icons/">
+        https://material-ui.com/components/icons/
+      </a>
       <ChoseIconWidget />
     </div>
   );
