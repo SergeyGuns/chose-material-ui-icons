0549e21c3f00f7cb29805f8d9425d05223b582bf
Sergey Ganin <mcheguevara2@gmail.com>
2019-07-18 20:20:07 +0300

    fix

---
+++ docs/precache-manifest.fc67fb00b6075d28f0f1506d97de86de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b25913687ac780ba1eee92181a40ad7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c39e8f1c78cb7296f45d",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "bf55e0b19b8b61de3d92",
+    "url": "./static/js/2.09eeb7d3.chunk.js"
+  },
+  {
+    "revision": "c39e8f1c78cb7296f45d",
+    "url": "./static/js/main.41c17fd6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import ChoseIconWidget from "./ChoseIconWidget";
 function App() {
   return (
     <div className="App">
+      <a href="https://github.com/SergeyGuns/chose-material-ui-icons">
+        <img
+          style={{ position: "absolute", top: 0, right: 0 }}
+          width="149"
+          height="149"
+          src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149"
+          class="attachment-full size-full"
+          alt="Fork me on GitHub"
+          data-recalc-dims="1"
+        />
+      </a>
       <a target="_blank" href="https://material-ui.com/api/icon/">
         https://material-ui.com/api/icon/
       </a>
